b5f5b46f69959a4fa85f691e5bafb921e4c99bac
yahiaT <yahiaahmedtol@gmail.com>
2023-05-22 22:04:32 +0300

    added comment i did this change

--- TheLastOfUs2/Milestone2-Solution/src/model/characters/Fighter.java
+++ TheLastOfUs2/Milestone2-Solution/src/model/characters/Fighter.java
@@ -5,6 +5,8 @@ public class Fighter extends Hero {
 	public Fighter(String name, int maxHp, int attackDamage, int maxActions) {
 		super(name, maxHp, attackDamage, maxActions);
 		//fighter will fight
+
+		//i did this change
 	}
 
 }
