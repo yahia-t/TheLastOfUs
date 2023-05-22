74d9506f1e5fad7f6573c39edabe2ef988d498cf
yahiaT <yahiaahmedtol@gmail.com>
2023-05-22 21:21:55 +0300

    testing

--- TheLastOfUs2/Milestone2-Solution/src/model/characters/Fighter.java
+++ TheLastOfUs2/Milestone2-Solution/src/model/characters/Fighter.java
@@ -4,6 +4,7 @@ public class Fighter extends Hero {
 
 	public Fighter(String name, int maxHp, int attackDamage, int maxActions) {
 		super(name, maxHp, attackDamage, maxActions);
+		//fighter will fight
 	}
 
 }
